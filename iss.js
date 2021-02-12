366e7661752b9d47eaaa81eedcc2a49c33cd747a
hansori01 <19sorihan88@gmail.com>
2021-02-12 00:50:33 +0000

    added request and access to ipify to rectrieve ip address

--- iss.js
+++ iss.js
@@ -8,9 +8,24 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = callback => { 
-  // use request to fetch IP address from JSON API
-  
-}
+const request = require('request');
+
+
+const fetchMyIP = callback => {
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    } if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const ip = JSON.parse(body).ip;
+      console.log('Status Code: ' + response.statusCode);
+      callback(null, ip);
+    }
+  });
+};
 
 module.exports = { fetchMyIP };
