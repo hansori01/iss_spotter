be56201067aa8796d5bb6f3caadd6a4f419977c9
hansori01 <19sorihan88@gmail.com>
2021-02-12 00:53:23 +0000

    refactored to simplify first if statement

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // require and run our main fetch function
 const { fetchMyIP } = require('./iss');
 
+// invokes function with callback function that relays the msg depending on ip result
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
+  
   console.log('It worked! Returned IP:', ip);
 });
--- iss.js
+++ iss.js
@@ -1,30 +1,20 @@
-// contains most of the logic for fetching the data from each API endpoint.
-
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
 
 const fetchMyIP = callback => {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    } if (response.statusCode !== 200) {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
-    } else {
-      const ip = JSON.parse(body).ip;
-      console.log('Status Code: ' + response.statusCode);
-      callback(null, ip);
     }
+
+    const ip = JSON.parse(body).ip;
+    console.log('Status Code: ' + response.statusCode);
+    callback(null, ip);
+
   });
 };
 
